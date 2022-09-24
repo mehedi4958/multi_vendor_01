5bd01cdce0b5cc903a8182c0698a8e617f270bef
Mehedi <mehedi@augnitive.com>
2022-09-25 00:06:01 +0600

    MVC structure created and boilerplate project cleaned up

---
+++ lib/views/landing_customer_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingCustomerScreen extends StatelessWidget {
+  const LandingCustomerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Landing Page'),
+      ),
+    );
+  }
+}
