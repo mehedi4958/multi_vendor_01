87cbbb223058f4585f290307c2d077c3d338dbe6
Mehedi <mehedi@augnitive.com>
2022-09-25 03:59:32 +0600

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_01/firebase_options.dart';
 import 'package:multi_vendor_01/views/auth/landing_customer_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
