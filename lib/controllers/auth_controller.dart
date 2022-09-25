17bad57c3137e9f0b80dd9cb3915c9953421ca45
Mehedi <mehedi@augnitive.com>
2022-09-25 06:20:40 +0600

    user info adding to the firestore while creating an account in firebase authentication

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,15 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<String> signUpUsers(
       String fullName, String email, String password) async {
     String res = 'some error occurred';
     try {
       if (fullName.isNotEmpty && email.isNotEmpty && password.isNotEmpty) {
-        await _auth.createUserWithEmailAndPassword(
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'fullName': fullName,
+          'email': email,
+        });
         res = 'success';
         print('Account Created');
       } else {
