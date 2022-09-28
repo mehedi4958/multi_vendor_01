e7c1e25fd3838ad65f3662f288a11dc41318e496
Mehedi <mehedi@augnitive.com>
2022-09-28 12:20:23 +0600

    Implementing Future Builder in Profile scrren to fetch and show user data from firestore

--- lib/views/customer_home_screen.dart
+++ lib/views/customer_home_screen.dart
@@ -11,7 +11,7 @@ class CustomerHomeScreen extends StatefulWidget {
 }
 
 class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
-  int _selectedItem = 0;
+  int _selectedItem = 4;
 
   final _pages = <Widget>[
     const HomeScreen(),
--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -10,6 +12,26 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    CollectionReference customer =
+        FirebaseFirestore.instance.collection('users');
+    return FutureBuilder(
+      future: customer.doc(_auth.currentUser!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('something went wrong');
+        }
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return const Text('Document does not exist');
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+        }
+        return const Text('Loading');
+      },
+    );
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       body: CustomScrollView(
