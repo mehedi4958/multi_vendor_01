2ff7fa480ae1f1d9fb1e8f2c8158fb0cc1db0b18
Mehedi <mehedi@augnitive.com>
2022-10-02 10:11:56 +0600

    validator using Global FormKey provided in the landing seller's screen

--- lib/views/auth/landing_seller_screen.dart
+++ lib/views/auth/landing_seller_screen.dart
@@ -17,6 +17,8 @@ class LandingSellerScreen extends StatefulWidget {
 class _LandingSellerScreenState extends State<LandingSellerScreen> {
   final _authController = AuthController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   bool passwordObscurity = true;
   bool isLoading = false;
 
@@ -43,202 +45,232 @@ class _LandingSellerScreenState extends State<LandingSellerScreen> {
         padding: const EdgeInsets.all(10.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Create Seller\'s Account',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Brand-Regular',
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.person,
-                        size: 35,
-                        color: Colors.cyan,
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    _image != null
-                        ? CircleAvatar(
-                            radius: 60,
-                            backgroundColor: Colors.cyan,
-                            backgroundImage: MemoryImage(_image!),
-                          )
-                        : const CircleAvatar(
-                            radius: 60,
-                            backgroundColor: Colors.cyan,
-                          ),
-                    const SizedBox(width: 10),
-                    Column(
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.cyan,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(15),
-                              topRight: Radius.circular(15),
-                            ),
-                          ),
-                          child: IconButton(
-                            onPressed: () {
-                              pickImageFromCamera();
-                            },
-                            icon: const Icon(
-                              Icons.camera_alt,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.cyan,
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(15),
-                              bottomRight: Radius.circular(15),
-                            ),
-                          ),
-                          child: IconButton(
-                            onPressed: () {
-                              pickImageFromGallery();
-                            },
-                            icon: const Icon(
-                              Icons.photo,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        label: const Text('Full Name'),
-                        hintText: 'Enter your full name',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        label: const Text('Email'),
-                        hintText: 'Enter your email',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      obscureText: passwordObscurity,
-                      decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              passwordObscurity = !passwordObscurity;
-                            });
-                          },
-                          icon: passwordObscurity
-                              ? const Icon(Icons.visibility)
-                              : const Icon(Icons.visibility_off),
-                        ),
-                        label: const Text('Password'),
-                        hintText: 'Enter your password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Create Seller\'s Account',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Brand-Regular',
+                          fontStyle: FontStyle.italic,
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: MediaQuery.of(context).size.width - 60,
-                        height: 50,
-                        decoration: BoxDecoration(
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.person,
+                          size: 35,
                           color: Colors.cyan,
-                          borderRadius: BorderRadius.circular(15),
                         ),
-                        child: Center(
-                          child: isLoading
-                              ? const CircularProgressIndicator(
-                                  color: Colors.white,
-                                )
-                              : const Text(
-                                  'Sign up',
-                                  style: TextStyle(
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      _image != null
+                          ? CircleAvatar(
+                              radius: 60,
+                              backgroundColor: Colors.cyan,
+                              backgroundImage: MemoryImage(_image!),
+                            )
+                          : const CircleAvatar(
+                              radius: 60,
+                              backgroundColor: Colors.cyan,
+                            ),
+                      const SizedBox(width: 10),
+                      Column(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.cyan,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15),
+                              ),
+                            ),
+                            child: IconButton(
+                              onPressed: () {
+                                pickImageFromCamera();
+                              },
+                              icon: const Icon(
+                                Icons.camera_alt,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.cyan,
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(15),
+                                bottomRight: Radius.circular(15),
+                              ),
+                            ),
+                            child: IconButton(
+                              onPressed: () {
+                                pickImageFromGallery();
+                              },
+                              icon: const Icon(
+                                Icons.photo,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Column(
+                    children: [
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Full name must not be empty';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          label: const Text('Full Name'),
+                          hintText: 'Enter your full name',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email must not be empty';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          label: const Text('Email'),
+                          hintText: 'Enter your email',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email must not be empty';
+                          } else {
+                            return null;
+                          }
+                        },
+                        obscureText: passwordObscurity,
+                        decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                passwordObscurity = !passwordObscurity;
+                              });
+                            },
+                            icon: passwordObscurity
+                                ? const Icon(Icons.visibility)
+                                : const Icon(Icons.visibility_off),
+                          ),
+                          label: const Text('Password'),
+                          hintText: 'Enter your password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      GestureDetector(
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            print('Good');
+                          } else {
+                            print('Please, do the right thing.');
+                          }
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width - 60,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.cyan,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Center(
+                            child: isLoading
+                                ? const CircularProgressIndicator(
                                     color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
+                                  )
+                                : const Text(
+                                    'Sign up',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Already have an account?',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(
-                                context, SellerLoginScreen.routeName);
-                          },
-                          child: const Text('Login'),
-                        )
-                      ],
-                    ),
-                    const Text(
-                      'Or',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Create a Customer\'s Account',
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.w500,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Already have an account?',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                  context, SellerLoginScreen.routeName);
+                            },
+                            child: const Text('Login'),
+                          )
+                        ],
+                      ),
+                      const Text(
+                        'Or',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(
-                                context, LandingCustomerScreen.routeName);
-                          },
-                          child: const Text('Sign up'),
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Create a Customer\'s Account',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                  context, LandingCustomerScreen.routeName);
+                            },
+                            child: const Text('Sign up'),
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
