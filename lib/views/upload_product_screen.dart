0c6e78d0f649e788e80f2f80a54843ffdcfb4040
Mehedi <mehedi@augnitive.com>
2022-10-03 12:48:35 +0600

    validator to the text fields provided

--- lib/views/upload_product_screen.dart
+++ lib/views/upload_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_01/controllers/snack_bar_controller.dart';
 
 class UploadProductScreen extends StatefulWidget {
   const UploadProductScreen({Key? key}) : super(key: key);
@@ -8,104 +9,140 @@ class UploadProductScreen extends StatefulWidget {
 }
 
 class _UploadProductScreenState extends State<UploadProductScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    width: MediaQuery.of(context).size.width * 0.5,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      color: Colors.cyan,
+                      child: const Center(
+                        child: Text(
+                          'You have not\n\nPicked any Image',
+                          style: TextStyle(
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text('Category Here'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                  child: Divider(
                     color: Colors.cyan,
-                    child: const Center(
-                      child: Text(
-                        'You have not\n\nPicked any Image',
-                        style: TextStyle(
-                          fontSize: 17,
+                    thickness: 1.6,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.38,
+                    child: TextFormField(
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Price must not be empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Price',
+                        hintText: 'Add Price',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
                         ),
                       ),
                     ),
                   ),
-                  const Text('Category Here'),
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-                child: Divider(
-                  color: Colors.cyan,
-                  thickness: 1.6,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.38,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Price',
-                      hintText: 'Add Price',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Quantity must not be empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Quantity',
+                        hintText: 'Add Quantity',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Quantity',
-                      hintText: 'Add Quantity',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 1.0,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Product name must not be empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                      maxLength: 100,
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        labelText: 'Product Name',
+                        hintText: 'Enter product name',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 1.0,
-                  child: TextFormField(
-                    maxLength: 100,
-                    maxLines: 3,
-                    decoration: InputDecoration(
-                      labelText: 'Product Name',
-                      hintText: 'Enter product name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 1.0,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Product description must not be empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                      maxLength: 800,
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        labelText: 'Product Description',
+                        hintText: 'Write product description',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 1.0,
-                  child: TextFormField(
-                    maxLength: 800,
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      labelText: 'Product Description',
-                      hintText: 'Write product description',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -120,7 +157,13 @@ class _UploadProductScreenState extends State<UploadProductScreen> {
             ),
           ),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                print('Cool');
+              } else {
+                snackBar(context, 'Fields must not be empty');
+              }
+            },
             child: const Icon(Icons.upload),
           ),
         ],
