fc1fb64f3b67ac05a8438f68a655d50672a4e0d7
Mehedi <mehedi@augnitive.com>
2022-10-08 21:26:14 +0600

    men gallery file created

---
+++ lib/views/gallery/men_gallery_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MenGalleryScreen extends StatefulWidget {
+  const MenGalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MenGalleryScreen> createState() => _MenGalleryScreenState();
+}
+
+class _MenGalleryScreenState extends State<MenGalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
