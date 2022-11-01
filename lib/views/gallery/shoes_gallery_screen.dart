6982b365738e16ea40ec8be5a95fbc91f989d3c3
Mehedi <mehedi@augnitive.com>
2022-11-01 19:55:59 +0600

    shoes gallery screen added

---
+++ lib/views/gallery/shoes_gallery_screen.dart
@@ -0,0 +1,124 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+
+class ShoesGalleryScreen extends StatefulWidget {
+  const ShoesGalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShoesGalleryScreen> createState() => _ShoesGalleryScreenState();
+}
+
+class _ShoesGalleryScreenState extends State<ShoesGalleryScreen> {
+  final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance
+      .collection('products')
+      .where('mainCategory', isEqualTo: 'Shoes')
+      .snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _productsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.cyan,
+            ),
+          );
+        }
+
+        if (snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text(
+              'This Category\n\nhas no item yet',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+        }
+
+        return SingleChildScrollView(
+          child: StaggeredGridView.countBuilder(
+              shrinkWrap: true,
+              itemCount: snapshot.data!.docs.length,
+              crossAxisCount: 2,
+              itemBuilder: (context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(
+                        15,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                          ),
+                          child: Container(
+                            constraints: const BoxConstraints(
+                              minHeight: 100,
+                              maxHeight: 250,
+                            ),
+                            child: Image.network(
+                                snapshot.data!.docs[index]['productImages'][0]),
+                          ),
+                        ),
+                        Text(snapshot.data!.docs[index]['productName']),
+                        Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                snapshot.data!.docs[index]['price'].toString(),
+                                style: const TextStyle(
+                                  color: Colors.cyan,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.favorite_border_outlined,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+              staggeredTileBuilder: (context) => const StaggeredTile.fit(1)),
+        );
+        //   ListView(
+        //   children: snapshot.data!.docs
+        //       .map((DocumentSnapshot document) {
+        //         Map<String, dynamic> data =
+        //             document.data()! as Map<String, dynamic>;
+        //         return ListTile(
+        //           title: Text(data['productName']),
+        //         );
+        //       })
+        //       .toList()
+        //       .cast(),
+        // );
+      },
+    );
+  }
+}
