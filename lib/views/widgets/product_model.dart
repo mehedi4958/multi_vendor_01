d6f4b2351c1d45e6ae6f08783bda63ffd07dec44
Mehedi <mehedi@augnitive.com>
2022-11-02 20:26:02 +0600

    product detail screen created

--- lib/views/widgets/product_model.dart
+++ lib/views/widgets/product_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_01/views/detail/product_detail_screen.dart';
 
 class ProductModel extends StatelessWidget {
   const ProductModel({
@@ -10,53 +11,63 @@ class ProductModel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(
-            15,
-          ),
-        ),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-              ),
-              child: Container(
-                constraints: const BoxConstraints(
-                  minHeight: 100,
-                  maxHeight: 250,
-                ),
-                child: Image.network(products['productImages'][0]),
-              ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const ProductDetailScreen();
+          }),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              15,
             ),
-            Text(products['productName']),
-            Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    products['price'].toString(),
-                    style: const TextStyle(
-                      color: Colors.cyan,
-                    ),
+          ),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Container(
+                  constraints: const BoxConstraints(
+                    minHeight: 100,
+                    maxHeight: 250,
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.favorite_border_outlined,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
+                  child: Image.network(products['productImages'][0]),
+                ),
               ),
-            )
-          ],
+              Text(products['productName']),
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      products['price'].toString(),
+                      style: const TextStyle(
+                        color: Colors.cyan,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.favorite_border_outlined,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
