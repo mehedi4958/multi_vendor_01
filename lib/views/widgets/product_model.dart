950d3da855b8889480160c0a73eb026c5c8e1278
Mehedi <mehedi@augnitive.com>
2022-11-04 01:30:22 +0600

    badges added to products

--- lib/views/widgets/product_model.dart
+++ lib/views/widgets/product_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 import 'package:multi_vendor_01/views/detail/product_detail_screen.dart';
 
 class ProductModel extends StatelessWidget {
@@ -24,52 +25,73 @@ class ProductModel extends StatelessWidget {
       },
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(
-              15,
-            ),
-          ),
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-                child: Container(
-                  constraints: const BoxConstraints(
-                    minHeight: 100,
-                    maxHeight: 250,
-                  ),
-                  child: Image.network(products['productImages'][0]),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  15,
                 ),
               ),
-              Text(products['productName']),
-              Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      products['price'].toString(),
-                      style: const TextStyle(
-                        color: Colors.cyan,
-                      ),
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.favorite_border_outlined,
-                        color: Colors.red,
+                    child: Container(
+                      constraints: const BoxConstraints(
+                        minHeight: 100,
+                        maxHeight: 250,
                       ),
+                      child: Image.network(products['productImages'][0]),
                     ),
-                  ],
+                  ),
+                  Text(products['productName']),
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          products['price'].toString(),
+                          style: const TextStyle(
+                            color: Colors.cyan,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border_outlined,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              left: 10,
+              top: 10,
+              child: Badge(
+                toAnimate: true,
+                shape: BadgeShape.square,
+                badgeColor: Colors.cyan,
+                borderRadius: BorderRadius.circular(8),
+                badgeContent: const Text(
+                  'New Arrival',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
