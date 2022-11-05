6122603ded2e287df220e0cf6b2ecc7719722316
Mehedi <mehedi@augnitive.com>
2022-11-05 14:59:44 +0600

    layout designed for place order screen

--- lib/views/minor_screens/place_order_screen.dart
+++ lib/views/minor_screens/place_order_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PlaceOrderScreen extends StatelessWidget {
@@ -5,8 +7,94 @@ class PlaceOrderScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.cyan,
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    CollectionReference customer =
+        FirebaseFirestore.instance.collection('customers');
+    return FutureBuilder(
+      future: customer.doc(_auth.currentUser!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('something went wrong');
+        }
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return const Text('Document does not exist');
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          return SafeArea(
+            child: Scaffold(
+              backgroundColor: Colors.grey.shade200,
+              appBar: AppBar(
+                backgroundColor: Colors.grey.shade200,
+                elevation: 0,
+              ),
+              body: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 90,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              bottomSheet: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 40,
+                      width: MediaQuery.of(context).size.width * 0.90,
+                      decoration: BoxDecoration(
+                        color: Colors.cyan,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: const Center(
+                          child: Text(
+                            'Confirm Order',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+        return const Material(
+          child: Center(
+            child: CircularProgressIndicator(
+              color: Colors.cyan,
+            ),
+          ),
+        );
+      },
     );
   }
 }
