55989536a709ba71575b73765f86d3422af81bdb
Mehedi <mehedi@augnitive.com>
2022-11-06 14:03:11 +0600

    search screen working

--- lib/views/inner_screens/search_screens.dart
+++ lib/views/inner_screens/search_screens.dart
@@ -1,18 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   static const String routeName = 'SearchScreen';
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
   Widget build(BuildContext context) {
+    String searchInput = '';
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
         iconTheme: const IconThemeData(color: Colors.black),
-        title: const CupertinoSearchTextField(),
+        title: CupertinoSearchTextField(
+          onChanged: (value) {
+            setState(
+              () {
+                searchInput = value;
+              },
+            );
+          },
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('products').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Material(
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: Colors.cyan,
+                ),
+              ),
+            );
+          }
+          final searchResult = snapshot.data!.docs.where((e) {
+            return e['productName']
+                .toLowerCase()
+                .contains(searchInput)
+                .toLowerCase();
+          });
+          return ListView(
+            children: searchResult.map((e) {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Image.network(e['productImages'][0]),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(e['productName']),
+                          Text(
+                            e['price'].toStringAsFixed(2),
+                            style: const TextStyle(color: Colors.cyan),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
